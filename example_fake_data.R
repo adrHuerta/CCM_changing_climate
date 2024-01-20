8c8ea024152eaf781636c50c54fa10a2c56b5e16
adrHuerta <adrhuerta@gmail.com>
2024-01-20 20:12:43 +0100

    example

---
+++ example_fake_data.R
@@ -0,0 +1,47 @@
+rm(list = ls())
+source("src/climatrend.R")
+
+set.seed(10)
+
+data_df <- data.frame(values = rnorm(100, mean = 15, sd = 1) + c(1:100)/50, years = 1901:2000)
+climate_trend_out <- climatrend(t = data_df$years, y = data_df$values)
+
+data_df$CCM_trend <- climate_trend_out$trend
+data_df$CCM <- mean(data_df[data_df$years > 1981 & data_df$years < 2010, ]$values)
+data_df <- transform(data_df,
+                     anom_CCM_trend = values - CCM_trend,
+                     col_CCM_trend = ifelse(values > CCM_trend, "1", ifelse(values < CCM_trend, "2", "0")),
+                     anom_CCM = values - CCM,
+                     col_CCM = ifelse(values > CCM, "1", ifelse(values < CCM, "2", "0")))
+
+ggplot2::theme_set(ggplot2::theme_minimal())
+
+ggplot2::ggplot() +
+  ggplot2::geom_point(data = data_df, ggplot2::aes(x = years, y = values), size = 2, colour = "gray70") +
+ggplot2::geom_line(data = data_df, ggplot2::aes(x = years, y = values), colour = "gray50", alpha = .7) + 
+  ggplot2::geom_line(data = data_df, ggplot2::aes(x = years, y = CCM_trend), colour = "#009E73", linewidth = 1.5)  + 
+  ggplot2::ylab("Air temperature (°C)") + ggplot2::xlab("") + ggplot2::ggtitle("Changing Current Climate Mean") -> p1
+
+ggplot2::ggplot() +
+  ggplot2::geom_point(data = data_df, ggplot2::aes(x = years, y = values), size = 2, colour = "gray70") +
+  ggplot2::geom_line(data = data_df, ggplot2::aes(x = years, y = values), colour = "gray50", alpha = .7) + 
+  ggplot2::geom_hline(yintercept =  unique(data_df$CCM), colour = "#009E73", linewidth = 1.5)   + 
+  ggplot2::ylab("Air temperature (°C)") + ggplot2::xlab("") + ggplot2::ggtitle("No Changing Current Climate Mean") -> p2
+
+
+ggplot2::ggplot() +
+  ggplot2::geom_bar(data = data_df, ggplot2::aes(x = years, y = anom_CCM, fill = col_CCM), stat = "identity") +
+  ggplot2::theme(legend.position = "none") + ggplot2::xlab("") + ggplot2::ylab("") +
+  ggplot2::scale_fill_manual(values = c("#D55E00", "#56B4E9")) +
+  ggplot2::scale_y_continuous(limits = c(-4.15, 4.15)) + ggplot2::ggtitle("Deviation with No Changing Current Climate Mean") -> p3
+
+
+ggplot2::ggplot() +
+  ggplot2::geom_bar(data = data_df, ggplot2::aes(x = years, y = anom_CCM_trend, fill = col_CCM_trend), stat = "identity")  +
+  ggplot2::scale_fill_manual(values = c("#D55E00", "#56B4E9")) +
+  ggplot2::theme(legend.position = "none") + ggplot2::xlab("") + ggplot2::ylab("") +
+  ggplot2::scale_y_continuous(limits = c(-4.15, 4.15))  + ggplot2::ggtitle("Deviation with Changing Current Climate Mean") -> p4
+
+
+cowplot::plot_grid(p2, p3, p1, p4, ncol = 2)
+
